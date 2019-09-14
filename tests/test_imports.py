ddf142ad69733683fab4e57eefe806eb38b09609
Anastasia Baryshnikova <abaryshnikova@calicolabs.com>
2019-09-13 19:03:58 -0700

    added the unittest for attribute file import

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -1,7 +1,9 @@
 import unittest
+import copy
 import networkx as nx
+import numpy as np
 
-from safepy import safe_io
+from safepy import safe, safe_io
 
 
 class TestImportCys(unittest.TestCase):
@@ -16,6 +18,28 @@ class TestImportCys(unittest.TestCase):
         self.assertEqual(num_nodes, 3971, "Should be 3971.")
         num_edges = nx.number_of_edges(self.graph)
         self.assertEqual(num_edges, 28202, "Should be 28202.")
+
+
+class TestImportAttributes(unittest.TestCase):
+
+    def setUp(self):
+        # Load the default network
+        sf = safe.SAFE(verbose=False)
+        sf.load_network()
+
+        self.sf = sf
+
+    def test_default(self):
+
+        sf = copy.deepcopy(self.sf)
+        sf.load_attributes()
+
+    def test_attribute_with_duplicate_values(self):
+
+        sf = copy.deepcopy(self.sf)
+
+        f = '/Users/abaryshnikova/Lab/Datasets/safe-data/attributes/sample_annotation_file.txt'
+        sf.load_attributes(attribute_file=f)
 
 
 if __name__ == '__main__':
