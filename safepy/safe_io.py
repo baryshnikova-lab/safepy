5d7ee85902bb5d948b7d8922098cdb1f986c7077
Anastasia Baryshnikova <asia.baryshnikova@gmail.com>
2023-12-19 14:32:26 +0100

    Enable composite network plotting for edgeless networks
    - Added a pseudo-network (based on euclidean distances) to edgeless networks (e.g., scatter plots) to facilitate downstream analysis.
    - Added node_size and alpha options to plot_network
    - Added show_network_contour to plot_composite_network and plot_sample_attributes
    - Suppressed FutureWarnings
    - Multiple commenting and formatting edits

--- safepy/safe_io.py
+++ safepy/safe_io.py
@@ -23,6 +23,9 @@ from scipy.optimize import fmin
 from collections import Counter
 from xml.dom import minidom
 
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
 
 def load_network_from_txt(filename, layout='spring_embedded', node_key_attribute='key', verbose=True):
     """
@@ -39,49 +42,44 @@ def load_network_from_txt(filename, layout='spring_embedded', node_key_attribute
     """
 
     filename = re.sub('~', expanduser('~'), filename)
-    
+
     # Get the number of columns first
-    if not Path(filename).suffix=='.gz':
+    if not Path(filename).suffix == '.gz':
         with open(filename, 'r') as f:
             first_line = f.readline()
             num_cols = len(first_line.split('\t'))
     else:
-        ## extract the file
+        # Extract the file
         import gzip
-        with gzip.open(filename,'rt') as f:
+        with gzip.open(filename, 'rt') as f:
             first_line = f.readline()
             num_cols = len(first_line.split('\t'))
-            
-    ## parameters provided to `pd.read_table`
-    if Path(filename).suffixes[0]=='.txt':
-        kws_read_table=dict(header=None,
-                            # names=None,
-                           )
-    elif Path(filename).suffixes[0]=='.tsv':
-        kws_read_table=dict(
-                            header=0, # column names in the first line
-                            names=range(num_cols),
-                           )    
+
+    # Parameters provided to `pd.read_table`
+    if Path(filename).suffixes[0] == '.txt':
+        kws_read_table = dict(header=None)
+    elif Path(filename).suffixes[0] == '.tsv':
+        kws_read_table = dict(
+            header=0,  # column names in the first line
+            names=range(num_cols),
+        )
     else:
         raise ValueError(f'extension {Path(filename).suffixes[0]} not supported')
-            
+
     if num_cols == 3:
 
-        data = pd.read_table(filename, sep='\t', 
+        data = pd.read_table(filename, sep='\t',
                              dtype={0: str, 1: str, 2: float},
-                             **kws_read_table,
-                            )        
+                             **kws_read_table
+                             )
         data = data.rename(columns={0: 'node_key1', 1: 'node_key2', 2: 'edge_weight'})
         data['node_label1'] = data['node_key1']
         data['node_label2'] = data['node_key2']
 
     elif num_cols == 5:
 
-        data = pd.read_table(filename, sep='\t', 
-                             # dtype={0: str, 1: str, 2: str, 3: str, 4: float},
-                             **kws_read_table,
-                            )
-        
+        data = pd.read_table(filename, sep='\t', **kws_read_table)
+
         data = data.rename(
             columns={0: 'node_label1', 1: 'node_key1', 2: 'node_label2', 3: 'node_key2', 4: 'edge_weight'})
 
@@ -176,7 +174,7 @@ def load_network_from_cys(filename, view_name=None, verbose=True):
         view_file = view_files[0]
 
     if verbose:
-        logging.info('Loading the view: %s' % viewfile)
+        logging.info('Loading the view: %s' % view_file)
 
     mydoc = minidom.parse(view_file)
     nodes = mydoc.getElementsByTagName('node')
@@ -382,7 +380,7 @@ def read_attributes(attribute_file='', node_label_order=None, mask_duplicates=Fa
     if not node2attribute.index.is_unique:
         logging.info('\nThe attribute file contains multiple values for the same labels. Their values will be averaged.')
         node2attribute = node2attribute.groupby(node2attribute.index, axis=0).mean()
-        
+
     if not node_label_order:
         node_label_order = node2attribute.index.values
 
@@ -400,8 +398,8 @@ def read_attributes(attribute_file='', node_label_order=None, mask_duplicates=Fa
 
         num_dups = mask_dups.sum()
         logging.info('\nThe network contains %d nodes with duplicate labels. '
-              'Only one random node per label will be considered. '
-              'The attribute values of all other nodes will be set to NaN.' % num_dups)
+                     'Only one random node per label will be considered. '
+                     'The attribute values of all other nodes will be set to NaN.' % num_dups)
         node2attribute.iloc[idx[mask_dups], :] = np.nan
 
     node2attribute = node2attribute.values
@@ -427,14 +425,15 @@ def read_attributes(attribute_file='', node_label_order=None, mask_duplicates=Fa
     return attributes, node_label_order, node2attribute
 
 
-def plot_network(
-    G,
-    ax=None,
-    foreground_color = '#ffffff',
-    background_color='#000000',
-    random_sampling_edges_min=30000,
-    title='Network',
-    ):
+def plot_network(G,
+                 ax=None,
+                 foreground_color='#ffffff',
+                 background_color='#000000',
+                 random_sampling_edges_min=30000,
+                 title='Network',
+                 node_size=10,
+                 alpha=0.2
+                 ):
     """
     Plot/draw a network.
     
@@ -443,6 +442,7 @@ def plot_network(
         gene ids: label_orf
         gene symbols: label
     """
+
     if background_color == '#ffffff':
         foreground_color = '#000000'
 
@@ -455,11 +455,12 @@ def plot_network(
     # Randomly sample a fraction of the edges (when network is too big)
     edges = tuple(G.edges())
     if len(edges) >= random_sampling_edges_min:
-        logging.warning(f"Edges are randomly sampled because the network (edges={len(edges)}) is too big (random_sampling_edges_min={random_sampling_edges_min}).")
+        logging.warning(f"Edges are randomly sampled because the network (edges={len(edges)}) is too \
+        big (random_sampling_edges_min={random_sampling_edges_min}).")
         edges = random.sample(edges, int(len(edges)*0.1))
 
     nx.draw(G, ax=ax, pos=node_xy, edgelist=edges,
-            node_color=foreground_color, edge_color=foreground_color, node_size=10, width=1, alpha=0.2)
+            node_color=foreground_color, edge_color=foreground_color, node_size=node_size, width=1, alpha=alpha)
 
     ax.set_aspect('equal')
     ax.set_facecolor(background_color)
@@ -523,9 +524,13 @@ def plot_network_contour(graph, ax, background_color='#000000'):
     return xf, yf, rf
 
 
-def plot_costanzo2016_network_annotations(
-    graph, ax, path_to_data, colors=True, clabels=False,
-    foreground_color='#ffffff', background_color='#000000'):
+def plot_costanzo2016_network_annotations(graph,
+                                          ax,
+                                          path_to_data,
+                                          colors=True,
+                                          clabels=False,
+                                          foreground_color='#ffffff',
+                                          background_color='#000000'):
 
     if background_color == '#ffffff':
         foreground_color = '#000000'
@@ -576,19 +581,17 @@ def plot_costanzo2016_network_annotations(
             logging.info('%d -- %s' % (n_process+1, process))
 
 
-def mark_nodes(
-    x,
-    y,
-    kind: list,
-    ax=None,
-    foreground_color='#ffffff',
-    background_color='#000000',
-    labels=None, # subset the nodes by labels
-    label_va='center',
-    legend_label: str=None,
-    test=False,
-    **kws,
-    ):
+def mark_nodes(x, y,
+               kind: list,
+               ax=None,
+               foreground_color='#ffffff',
+               background_color='#000000',
+               labels=None,  # subset the nodes by labels
+               label_va='center',
+               legend_label: str = None,
+               test=False,
+               **kws,
+               ):
     """
     Show nodes.
 
@@ -596,27 +599,32 @@ def mark_nodes(
         s (str): legend name (defaults to '').
         kind (str): 'mark' if the nodes should be marked, 'label' if nodes should be marked and labeled.
     """
+
     if ax is None:
-        ax = plt.gca() # get current axes i.e. subplot
+        ax = plt.gca()  # get current axes i.e. subplot
+
     if isinstance(kind, str):
         kind = [kind]
-        
+
     if 'mark' in kind:
-        ## mark the selected nodes with the marker +
+        # Mark the selected nodes with the marker +
         sn1 = ax.scatter(x, y, **kws)
 
     if 'label' in kind:
-        ## show labels e.g. gene names
-        if test:print(x,y,labels)
-        assert len(x)==len(labels), f"len(x)!=len(labels): {len(x)}!={len(labels)}"
-        if test:ax.plot(x, y, 'r*')
-        for i,label in enumerate(labels):
+        # Show labels e.g. gene names
+        if test: print(x, y, labels)
+
+        assert len(x) == len(labels), f"len(x)!=len(labels): {len(x)}!={len(labels)}"
+
+        if test: ax.plot(x, y, 'r*')
+
+        for i, label in enumerate(labels):
             ax.text(x[i], y[i], label,
-                    fontdict={'color': 'white' if background_color== '#000000' else 'k', 'size': 14, 'weight': 'bold'},
+                    fontdict={'color': 'white' if background_color == '#000000' else 'k', 'size': 14, 'weight': 'bold'},
                     # bbox={'facecolor': 'black', 'alpha': 0.5, 'pad': 3},
                     ha='center',
-                    va=label_va,
-                   )
+                    va=label_va
+                    )
 
     if not legend_label is None:
         # Legend
@@ -629,30 +637,30 @@ def mark_nodes(
 
         leg_title = leg.get_title()
         leg_title.set_color(foreground_color)
-        
+
     return ax
 
 
-def get_node_coordinates(graph,labels=[]):
+def get_node_coordinates(graph, labels=[]):
 
     x = dict(graph.nodes.data('x'))
-    y = dict(graph.nodes.data('y'))    
+    y = dict(graph.nodes.data('y'))
 
     ds = [x, y]
     pos = {}
     for k in x:
         pos[k] = np.array([d[k] for d in ds])
 
-    node_xy_list=list(pos.values())
+    node_xy_list = list(pos.values())
 
-    if len(labels)==0:
-        return  np.vstack(node_xy_list)    
+    if len(labels) == 0:
+        return np.vstack(node_xy_list)
     else:
 
         # Get the co-ordinates of the nodes
         node_labels = nx.get_node_attributes(graph, 'label')
         node_labels_dict = {k: v for v, k in node_labels.items()}
-        
+
         # TODOs: avoid determining the x and y again.
         x = list(dict(graph.nodes.data('x')).values())
         y = list(dict(graph.nodes.data('y')).values())
@@ -670,14 +678,14 @@ def get_node_coordinates(graph,labels=[]):
         labels_missing = [x for x in labels if x not in node_labels_dict.keys()]
         if labels_missing:
             labels_missing_str = ', '.join(labels_missing)
-            logging.warning('These labels are missing from the network (case sensitive): %s' % labels_missing_str)    
-        
-        node_xy_list=[x_idx,y_idx]
-        
+            logging.warning('These labels are missing from the network (case sensitive): %s' % labels_missing_str)
+
+        node_xy_list = [x_idx, y_idx]
+
         return np.vstack(node_xy_list).T, labels_found
 
 
-## io
+# io
 def load_mat(filename):
     """
     this function should be called instead of direct spio.loadmat
@@ -730,7 +738,3 @@ def chop_and_filter(s):
     single_list_words = [w for w in single_list_count if w not in to_exclude]
 
     return ', '.join(single_list_words[:5])
-
-
-
-
